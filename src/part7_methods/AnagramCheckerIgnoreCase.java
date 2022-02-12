d27f835140e32cbac473bc1d3eb942ceff3753dd
HakanK003 <hkaratashk3@gmail.com>
2022-02-12 12:52:47 -0500

    patch 7 methods

--- src/part7_methods/AnagramCheckerIgnoreCase.java
+++ src/part7_methods/AnagramCheckerIgnoreCase.java
@@ -1,9 +1,10 @@
-package methods;
+package part7_methods;
 
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class AnagramCheckerIgnoreCase {
+
     public static boolean isAnagram(String word1, String word2) {
 
         boolean a =false;
@@ -37,3 +38,20 @@ public class AnagramCheckerIgnoreCase {
     }
 
 }
+
+/*
+Anagram is a word, phrase, or name formed by rearranging the letters of another, such as cinema, formed from iceman.
+
+The isAnagram method checks if word1 and word2 are anagram to each other than return a boolean.
+
+each letter in word1 should appear in word2 exact number of times
+ignore empty spaces
+make your code case-insensitive
+Examples:
+
+isAnagram("listen", "Silent") ==> true
+isAnagram("earth", "heart") ==> true
+isAnagram("star", "rats") ==> true
+isAnagram("hi", "bye") ==> false
+isAnagram("java", "cava") ==> false
+ */
--- src/part7_methods/Overloading1.java
+++ src/part7_methods/Overloading1.java
@@ -1,22 +1,23 @@
-package methods;
+package part7_methods;
 
 import java.util.Scanner;
 
 public class Overloading1 {
 
     public static int findMax(int[] arr){
+
         int max=arr[0];
         for(int each : arr){if(each>max){max=each;}}
         return max;
     }
+
     public static double findMax(double[] arr){
+
         double max=arr[0];
         for(double each : arr){if(each>max){max=each;}}
         return max;
     }
 
-
-// DO NOT TOUCH THE MAIN METHOD
 
     public static void main(String [] args) {
 
@@ -31,12 +32,22 @@ public class Overloading1 {
             }
             System.out.println(findMax(arr));
         } else {
-            double [] arr = new double[size];
+            double[] arr= new double[size];
             for(int i=0; i < arr.length; i++) {
                 arr[i] = in.nextDouble();
             }
             System.out.println(findMax(arr));
+            in.close();
         }
 
     }
+
 }
+
+/*
+In this task, you need to write 2 overloaded methods findMax() that will find a maximum number in the array.
+First version should work with array of integers (int[]) and return int,
+and second method should work with an array of doubles (double[]) and return double as a result.
+
+Methods must have the same name and different parameters.
+ */
